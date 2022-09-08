bc82d485ca8032975567b380d5c72fd96e5d9888
@prathiksha_vinove <prathiksha.shetty@mail.vinove.com>
2022-09-08 17:48:49 +0530

    validation added

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  // res.status(500).send('File already exists');
+
+
+
   res.render('error');
 });
 
